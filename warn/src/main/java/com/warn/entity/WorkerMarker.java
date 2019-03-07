cb65f29d2af53741a8c707759dbee5637e199143
MistQ <42373544+MistQ@users.noreply.github.com>
2019-03-07 11:14:58 +0800

    Update WorkerMarker.java

--- warn/src/main/java/com/warn/entity/WorkerMarker.java
+++ warn/src/main/java/com/warn/entity/WorkerMarker.java
@@ -1,13 +1,16 @@
 package com.warn.entity;
 
-import javax.xml.crypto.Data;
+import java.util.Date;
 
 public class WorkerMarker {
     private Integer id;
     private String cx;//坐标
     private String cy;//坐标
-    private Data time;//更新时间
+    private Date time;//更新时间
     private Integer taskid;
+    private String name;
+    private String qq;
+    private String phone;
 
     public Integer getId() {
         return id;
@@ -33,11 +36,11 @@ public class WorkerMarker {
         this.cy = cy;
     }
 
-    public Data getTime() {
+    public Date getTime() {
         return time;
     }
 
-    public void setTime(Data time) {
+    public void setTime(Date time) {
         this.time = time;
     }
     
@@ -47,5 +50,29 @@ public class WorkerMarker {
 
     public void setTaskid(Integer taskid) {
         this.taskid = taskid;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getQq() {
+        return qq;
+    }
+
+    public void setQq(String qq) {
+        this.qq = qq;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 }
